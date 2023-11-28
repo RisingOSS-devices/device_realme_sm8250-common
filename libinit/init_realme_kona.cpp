897af923ace4334831b9f5be0c6bf2400bf2284c
aamirr-ali <amirwide@gmail.com>
2023-11-28 23:46:53 +0530

    sm8250-common:Add properties for `MATRIXX`

--- libinit/init_realme_kona.cpp
+++ libinit/init_realme_kona.cpp
@@ -7,6 +7,25 @@
 #include <libinit_variant.h>
 
 #include "vendor_init.h"
+#include <cstdlib>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
+#include <android-base/properties.h>
+#include "property_service.h"
+
+using android::base::GetProperty;
+using std::string;
+
+void property_override(string prop, string value)
+{
+    auto pi = (prop_info*) __system_property_find(prop.c_str());
+
+    if (pi != nullptr)
+        __system_property_update(pi, value.c_str(), value.size());
+    else
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+}
 
 static const variant_info_t RMX2071_CN_info = {
     .prjversion_value = "19795",
@@ -14,7 +33,6 @@ static const variant_info_t RMX2071_CN_info = {
     .device = "RMX2071CN",
     .model = "RMX2071",
     .name = "RMX2071",
-
     .sku = "bladerunner",
 };
 
@@ -24,7 +42,6 @@ static const variant_info_t RMX2072_CN_info = {
     .device = "RMX2072CN",
     .model = "RMX2072",
     .name = "RMX2072",
-
     .sku = "bladerunner_48m",
 };
 
@@ -34,7 +51,6 @@ static const variant_info_t RMX2075_GLOBAL_info = {
     .device = "RMX2075L1",
     .model = "RMX2075",
     .name = "RMX2075",
-
     .sku = "bladerunner",
 };
 
@@ -44,7 +60,6 @@ static const variant_info_t RMX2076_IN_info = {
     .device = "RMX2076L1",
     .model = "RMX2076",
     .name = "RMX2076",
-
     .sku = "bladerunner",
 };
 
@@ -54,8 +69,6 @@ static const variant_info_t RMX3370_CN_info = {
     .device = "RE5473",
     .model = "RMX3370",
     .name = "RMX3370",
-
-    .sku = "bitra",
 };
 
 static const variant_info_t RMX3370_CNLZ_info = {
@@ -64,8 +77,6 @@ static const variant_info_t RMX3370_CNLZ_info = {
     .device = "RE5473",
     .model = "RMX3370",
     .name = "RMX3370",
-
-    .sku = "bitra",
 };
 
 static const variant_info_t RMX3370_GLOBAL_info = {
@@ -74,8 +85,6 @@ static const variant_info_t RMX3370_GLOBAL_info = {
     .device = "RE879AL1",
     .model = "RMX3370",
     .name = "RMX3370",
-
-    .sku = "bitra",
 };
 
 static const variant_info_t RMX3370_IN_info = {
@@ -84,8 +93,6 @@ static const variant_info_t RMX3370_IN_info = {
     .device = "RE879AL1",
     .model = "RMX3370",
     .name = "RMX3370",
-
-    .sku = "bitra",
 };
 
 static const std::vector<variant_info_t> variants = {
@@ -100,4 +107,9 @@ static const std::vector<variant_info_t> variants = {
 
 void vendor_load_properties() {
     search_variant(variants);
+    
+    // Misc
+    property_override("ro.matrixx.chipset", "Snapdragon® 865 5G");
+    property_override("ro.matrixx.device", "Realme X50 Pro");
+    property_override("ro.matrixx.maintainer", "AAMIRR ALI");
 }
