51dc227be14a25bcd47e24d349ab82c997a226b0
aamirr-ali <amirwide@gmail.com>
2024-04-20 23:46:11 +0530

    sm8250-common: dolby: Fix compile error

--- DolbyManager/src/org/lineageos/settings/SummaryProvider.java
+++ DolbyManager/src/org/lineageos/settings/SummaryProvider.java
@@ -15,7 +15,7 @@
  * limitations under the License
  */
 
-package co.aospa.settings.dolby;
+package com.aospa.dolbymanager;
 
 import static com.android.settingslib.drawer.TileUtils.META_DATA_PREFERENCE_SUMMARY;
 
@@ -25,8 +25,8 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 
-import com.android.settingslib.widget.R;
-import co.aospa.settings.dolby.DolbyUtils;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.aospa.dolbymanager.DolbyUtils;
 
 /** Provide preference summary for injected items. */
 public class SummaryProvider extends ContentProvider {
@@ -83,13 +83,13 @@ public class SummaryProvider extends ContentProvider {
         final DolbyUtils dolbyUtils = DolbyUtils.getInstance(getContext());
         final boolean dsOn = dolbyUtils.getDsOn();
         if (!dsOn) {
-            return getContext().getString(R.string.dolby_off);
+            return getContext().getString(com.android.settingslib.collapsingtoolbar.R.string.dolby_off);
         }
         final String profileName = dolbyUtils.getProfileName();
         if (profileName == null) {
-            return getContext().getString(R.string.dolby_on);
+            return getContext().getString(com.android.settingslib.collapsingtoolbar.R.string.dolby_on);
         } else {
-            return getContext().getString(R.string.dolby_on_with_profile, profileName);
+            return getContext().getString(com.android.settingslib.collapsingtoolbar.R.string.dolby_on_with_profile, profileName);
         }
     }
 }
